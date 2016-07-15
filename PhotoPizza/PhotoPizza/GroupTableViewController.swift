8153ec4bc99e62bfc57a6fb38b3807c77a70f9ca
gary cheng <garycheng@berkeley.edu>
2016-07-15 18:41:33 +0200

    avatars load, but not all the groups load

--- PhotoPizza/PhotoPizza/GroupTableViewController.swift
+++ PhotoPizza/PhotoPizza/GroupTableViewController.swift
@@ -67,7 +67,7 @@ class GroupTableViewController: UITableViewController, UINavigationControllerDel
             print("json count: \(json.count)")
             for (_, newDict):(String, JSON) in json {
                 let groupName = newDict["groupName"].stringValue
-                let avatarImgId = newDict["avatarId"].stringValue
+                let avatarImgId = newDict["avatarImgId"].stringValue
                 
                 let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
                 self.groups.append(newGroup)
@@ -111,7 +111,7 @@ class GroupTableViewController: UITableViewController, UINavigationControllerDel
                     return
                 }
             }
-            let avatarImgId = newDict["avatarId"] as! String
+            let avatarImgId = newDict["avatarImgId"] as! String
             let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
             self.groups.append(newGroup)
             let photoRef = storageRef.child("images/" + avatarImgId)
