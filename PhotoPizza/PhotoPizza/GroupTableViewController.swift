73e4b72be2acd084ddefbe3f69f8e2337a0b99e3
gary cheng <garycheng@berkeley.edu>
2016-07-18 13:46:16 +0200

    got the hardcoded groups to work

--- PhotoPizza/PhotoPizza/GroupTableViewController.swift
+++ PhotoPizza/PhotoPizza/GroupTableViewController.swift
@@ -28,9 +28,9 @@ class GroupTableViewController: UITableViewController, UINavigationControllerDel
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
-        //loadSampleMeals()
         initGroups()
         dbListen()
+        print("12341234THIS IS THE GORUP1234: \(currentUser.firebaseId)")
         navigationController!.navigationBar.barTintColor = UIColor(red:0.38, green:0.28, blue:0.62, alpha:1.0)
         navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red:0.93, green:0.93, blue:0.93, alpha:1.0)]
         self.tableView.backgroundColor = UIColor(red:0.9995, green:0.9995, blue:0.9995, alpha:1.0)
@@ -47,79 +47,104 @@ class GroupTableViewController: UITableViewController, UINavigationControllerDel
         //let groupRef = self.ref.child("groups")
         //let curGroupRef = groupRef.child(self.navigationItem.title!)
         let storageRef = storage.referenceForURL("gs://photo-pizza.appspot.com")
-        groups = [Group]()
-        ref.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
+        //groups = [Group]()
+        let userGroupRef = self.ref.child("users/" + currentUser.firebaseId)
+        
+        print("THIS IS THE 93281740: \(currentUser.firebaseId)")
+        
+        
+        userGroupRef.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
             //print("we made it here: \(self.navigationItem.title)")
-            let json = JSON(snapshot.value!)["groups"]
-            print(json)
-            print("json count: \(json.count)")
-            for (_, newDict):(String, JSON) in json {
-                let groupName = newDict["groupName"].stringValue
-                let avatarImgId = newDict["avatarImgId"].stringValue
+            let validGroups = JSON(snapshot.value!)["groups"]
+            //print(json)
+            //print("json count: \(json.count)")
+            //print (currentUser.groups)
+            for (group, _):(String, JSON) in validGroups {
                 
-                let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
-                self.groups.append(newGroup)
-                newGroup.update = newDict["update"].stringValue ?? ""
+                let validGroupRef = self.ref.child("groups/" + group)
+                validGroupRef.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
+                    let newDict = JSON(snapshot.value!)
+                    //print("HIGH: \(group)")
+                //for (_, newDict):(String, JSON) in json {
+                    //if let newDict: JSON = allGroups[group] {
+                    let groupName = newDict["groupName"].stringValue
+                    let avatarImgId = newDict["avatarImgId"].stringValue
+                    
+                    let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
+                    self.groups.append(newGroup)
+                    newGroup.update = newDict["update"].stringValue ?? ""
 
-                print("NEWVAL: \(groupName)")
-               
-                let photoRef = storageRef.child("images/" + avatarImgId)
-                
-                photoRef.dataWithMaxSize(1 * 4000 * 4000) { (data, error) -> Void in
-                    if (error != nil) {
-                        // Uh-oh, an error occurred!
-                        self.groupTableView.reloadData()
-                    } else {
-                        // Data for "images/island.jpg" is returned
-                        // ... let islandImage: UIImage! = UIImage(data: data!)
-                        newGroup.avatar = UIImage(data: data!)
-                        //self.loadView()
-                        
-                        self.groupTableView.reloadData()
-                        
+                    print("NEWVAL: \(groupName)")
+                   
+                    let photoRef = storageRef.child("images/" + avatarImgId)
+                    
+                    photoRef.dataWithMaxSize(1 * 4000 * 4000) { (data, error) -> Void in
+                        if (error != nil) {
+                            // Uh-oh, an error occurred!
+                            self.groupTableView.reloadData()
+                        } else {
+                            // Data for "images/island.jpg" is returned
+                            // ... let islandImage: UIImage! = UIImage(data: data!)
+                            newGroup.avatar = UIImage(data: data!)
+                            //self.loadView()
+                            
+                            self.groupTableView.reloadData()
+                            
+                        }
                     }
-                }
-                self.groupTableView.reloadData()
+                    self.groupTableView.reloadData()
+                })
             }
         })
+        
         
     }
     
     func dbListen() {
         let storageRef = storage.referenceForURL("gs://photo-pizza.appspot.com")
         //let postRef = FIRDatabase.database().reference().child("images")
-        let groupRef = self.ref.child("groups")
-        //let curGroupRef = groupRef.child(self.navigationItem.title!)
         
-        groupRef.observeEventType(.ChildAdded, withBlock: { (snapshot) in
-            let newDict = snapshot.value as! Dictionary<String, AnyObject>
-            let groupName = newDict["groupName"] as! String
-            for group in self.groups {
-                if group.name == groupName{
-                    return
-                }
-            }
-            let avatarImgId = newDict["avatarImgId"] as! String ?? ""
-            let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
-            self.groups.append(newGroup)
-            let photoRef = storageRef.child("images/" + avatarImgId)
+         let userGroupRef = self.ref.child("users/" + currentUser.firebaseId + "/groups")
+        
+        userGroupRef.observeEventType(.ChildAdded, withBlock: { (snapshot) in
             
-            photoRef.dataWithMaxSize(1 * 4000 * 4000) { (data, error) -> Void in
-                if (error != nil) {
-                    // Uh-oh, an error occurred!
-                    self.groupTableView.reloadData()
-                } else {
-                    // Data for "images/island.jpg" is returned
-                    // ... let islandImage: UIImage! = UIImage(data: data!)
-                    newGroup.avatar = UIImage(data: data!)
-                    self.groupTableView.reloadData()
+            let newDict = JSON(snapshot.value!)
+            for (groupAdded, _) in newDict {
+                let groupRef = self.ref.child("groups/" + groupAdded)
+
+                groupRef.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
                     
-                }
+                    let groupDict = snapshot.value as! Dictionary<String, AnyObject>
+                    let groupName = groupDict["groupName"] as! String
+                    for group in self.groups {
+                        if group.name == groupName{
+                            return
+                        }
+                    }
+                    let avatarImgId = groupDict["avatarImgId"] as! String ?? ""
+                    let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
+                    self.groups.append(newGroup)
+                    let photoRef = storageRef.child("images/" + avatarImgId)
+                    photoRef.dataWithMaxSize(1 * 4000 * 4000) { (data, error) -> Void in
+                        if (error != nil) {
+                            // Uh-oh, an error occurred!
+                            self.groupTableView.reloadData()
+                        } else {
+                            // Data for "images/island.jpg" is returned
+                            // ... let islandImage: UIImage! = UIImage(data: data!)
+                            newGroup.avatar = UIImage(data: data!)
+                            self.groupTableView.reloadData()
+                            
+                        }
+                    }
+                    self.groupTableView.reloadData()
+
+                })
             }
-            self.groupTableView.reloadData()
+
             
         })
-        groupRef.observeEventType(.ChildRemoved, withBlock: { (snapshot) in
+        userGroupRef.observeEventType(.ChildRemoved, withBlock: { (snapshot) in
             let newDict = snapshot.value as! Dictionary<String, AnyObject>
             let groupName = newDict["groupName"] as! String
             let len = self.groups.count
--- PhotoPizza/PhotoPizza/ViewController.swift
+++ PhotoPizza/PhotoPizza/ViewController.swift
@@ -16,6 +16,8 @@ import SwiftyJSON
 
 var currentUser : User = User()
 
+var isItDone = false
+
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     // MARK: Properties
@@ -84,16 +86,46 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
                 let email = json["email"].stringValue
                 
                 //TODO: make groups better
-                var groups = [String]()
-                groups.append("hello")
-                groups.append("goodbye")
-                groups.append("waterfall")
+                var groups = [String:String]()
+                groups["hello"] = "hello"
+                groups["goodbye"] = "goodbye"
                 
                 print("name: \(name)")
                 print("id: \(facebookId)")
                 print("email: \(email)")
                 
                 currentUser = User(name: name, email: email, facebookId: facebookId, groups: groups)
+               // print("THIS IS THE GORUP: \(currentUser.groups)")
+                
+                let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
+                
+                FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
+                    if (error != nil) {
+                        print(error)
+                    }
+                    else {
+                        currentUser.firebaseId = user!.uid
+                        
+                        let userRef = FIRDatabase.database().reference().child("users").child(currentUser.firebaseId)
+                        let userDict: [String: AnyObject] = ["facebookId": String(currentUser.facebookId),
+                            "firebaseId": currentUser.firebaseId,
+                            "userName": currentUser.name,
+                            "userEmail": currentUser.email,
+                            "groups": currentUser.groups]
+                        userRef.updateChildValues(userDict)
+                        //                self.initGroups()
+                        //                self.dbListen()
+                        isItDone = true
+                        
+                        let storyBoard : UIStoryboard? = self.storyboard
+                        
+                        let nextViewController = (storyBoard?.instantiateViewControllerWithIdentifier("newNav"))! as UIViewController
+                        self.presentViewController(nextViewController, animated:true, completion:nil)
+                        
+                    }
+                }
+
+
                 
             }
             else{
@@ -107,30 +139,9 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             return
         }
         
-        let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
         
-        FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
-            if (error != nil) {
-                print(error)
-            }
-            else {
-                currentUser.firebaseId = user!.uid
-                let userRef = FIRDatabase.database().reference().child("users").child(currentUser.firebaseId)
-                let userDict: [String: AnyObject] = ["facebookId": String(currentUser.facebookId),
-                                                  "firebaseId": currentUser.firebaseId,
-                                                  "userName": currentUser.name,
-                                                  "userEmail": currentUser.email,
-                                                  "groups": currentUser.groups]
-                userRef.updateChildValues(userDict)
-            }
-        }
-        
-        let storyBoard : UIStoryboard? = self.storyboard
-        
-        let nextViewController = (storyBoard?.instantiateViewControllerWithIdentifier("newNav"))! as UIViewController
-        //FBSDKLoginManager().logOut()
-        self.presentViewController(nextViewController, animated:true, completion:nil)
-//        let secondViewController = (self.storyboard?.instantiateViewControllerWithIdentifier("home"))! as UIViewController
+       
+        //        let secondViewController = (self.storyboard?.instantiateViewControllerWithIdentifier("home"))! as UIViewController
 //        self.navigationController?.pushViewController(secondViewController, animated: true)
     }
     
@@ -142,6 +153,126 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 //            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
 //        }
 //    }
+    
+//    // Listeners for the groups
+//    func initGroups() {
+//        //let groupRef = self.ref.child("groups")
+//        //let curGroupRef = groupRef.child(self.navigationItem.title!)
+//        let storageRef = storage.referenceForURL("gs://photo-pizza.appspot.com")
+//        //groups = [Group]()
+//        let userGroupRef = ref.child("users/" + currentUser.firebaseId)
+//        
+//        print("THIS IS THE 93281740: \(currentUser.firebaseId)")
+//        
+//        
+//        userGroupRef.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
+//            //print("we made it here: \(self.navigationItem.title)")
+//            let validGroups = JSON(snapshot.value!)["groups"]
+//            //print(json)
+//            //print("json count: \(json.count)")
+//            //print (currentUser.groups)
+//            for (group, _):(String, JSON) in validGroups {
+//                
+//                let validGroupRef = ref.child("groups/" + group)
+//                validGroupRef.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
+//                    let newDict = JSON(snapshot.value!)
+//                    //print("HIGH: \(group)")
+//                    //for (_, newDict):(String, JSON) in json {
+//                    //if let newDict: JSON = allGroups[group] {
+//                    let groupName = newDict["groupName"].stringValue
+//                    let avatarImgId = newDict["avatarImgId"].stringValue
+//                    
+//                    let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
+//                    groups.append(newGroup)
+//                    newGroup.update = newDict["update"].stringValue ?? ""
+//                    
+//                    print("NEWVAL: \(groupName)")
+//                    
+//                    let photoRef = storageRef.child("images/" + avatarImgId)
+//                    
+//                    photoRef.dataWithMaxSize(1 * 4000 * 4000) { (data, error) -> Void in
+//                        if (error != nil) {
+//                            // Uh-oh, an error occurred!
+//                            self.groupTableView.reloadData()
+//                        } else {
+//                            // Data for "images/island.jpg" is returned
+//                            // ... let islandImage: UIImage! = UIImage(data: data!)
+//                            newGroup.avatar = UIImage(data: data!)
+//                            //self.loadView()
+//                            
+//                            self.groupTableView.reloadData()
+//                            
+//                        }
+//                    }
+//                    self.groupTableView.reloadData()
+//                })
+//            }
+//        })
+//        
+//        
+//    }
+//    
+//    func dbListen() {
+//        let storageRef = storage.referenceForURL("gs://photo-pizza.appspot.com")
+//        //let postRef = FIRDatabase.database().reference().child("images")
+//        
+//        let userGroupRef = self.ref.child("users/" + currentUser.firebaseId + "/groups")
+//        
+//        userGroupRef.observeEventType(.ChildAdded, withBlock: { (snapshot) in
+//            
+//            let newDict = snapshot.value as! Dictionary<String, AnyObject>
+//            print("HIGHLY IMPORTANT SHIT: \(newDict)")
+//            for (groupAdded, _) in newDict {
+//                let groupRef = self.ref.child("groups/" + groupAdded)
+//                
+//                groupRef.queryOrderedByKey().observeSingleEventOfType(.Value, withBlock: { snapshot in
+//                    
+//                    let groupDict = snapshot.value as! Dictionary<String, AnyObject>
+//                    let groupName = groupDict["groupName"] as! String
+//                    for group in self.groups {
+//                        if group.name == groupName{
+//                            return
+//                        }
+//                    }
+//                    let avatarImgId = groupDict["avatarImgId"] as! String ?? ""
+//                    let newGroup = Group(name: groupName, avatar: UIImage(named: "noAvatar"))
+//                    self.groups.append(newGroup)
+//                    let photoRef = storageRef.child("images/" + avatarImgId)
+//                    photoRef.dataWithMaxSize(1 * 4000 * 4000) { (data, error) -> Void in
+//                        if (error != nil) {
+//                            // Uh-oh, an error occurred!
+//                            self.groupTableView.reloadData()
+//                        } else {
+//                            // Data for "images/island.jpg" is returned
+//                            // ... let islandImage: UIImage! = UIImage(data: data!)
+//                            newGroup.avatar = UIImage(data: data!)
+//                            self.groupTableView.reloadData()
+//                            
+//                        }
+//                    }
+//                    self.groupTableView.reloadData()
+//                    
+//                })
+//            }
+//            
+//            
+//        })
+//        userGroupRef.observeEventType(.ChildRemoved, withBlock: { (snapshot) in
+//            let newDict = snapshot.value as! Dictionary<String, AnyObject>
+//            let groupName = newDict["groupName"] as! String
+//            let len = self.groups.count
+//            for i in 0..<len {
+//                let curr = self.groups[i]
+//                if (curr.name == groupName) {
+//                    self.groups.removeAtIndex(i)
+//                    self.groupTableView.reloadData()
+//                    return
+//                }
+//            }
+//            print(self.groups)
+//        })
+//    }
+    
     
 
     
